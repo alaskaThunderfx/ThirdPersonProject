929e3da3fc7ae73bd18848c1cda055104463d2e5
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2023-01-20 03:19:09 -0500

    Add animator blend tree and script logic
    Makes the character animations play either idle or running animations
    based on whether or not they are moving

--- Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -8,6 +8,7 @@ namespace StateMachines.Player
         // Public variables
         [field: SerializeField] public InputReader InputReader { get; private set; }
         [field: SerializeField] public CharacterController CharacterController { get; private set; }
+        [field: SerializeField] public Animator Animator { get; private set; }
         [field: SerializeField] public float FreeLookMovementSpeed { get; private set; }
 
         // Unity built-in methods
