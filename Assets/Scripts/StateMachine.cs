b003c4c1901e9532ecd57c14888623cf52a839b2
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2023-01-20 01:16:12 -0500

    Add base scripts for State and State Machine

---
+++ Assets/Scripts/StateMachine.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public abstract class StateMachine : MonoBehaviour
+{
+    private State _currentState;
+
+    // Unity built-in methods
+    private void Update()
+    {
+        _currentState?.Tick(Time.deltaTime);
+    }
+
+    // Public methods
+    public void SwitchState(State newState)
+    {
+        _currentState?.Exit();
+        _currentState = newState;
+        _currentState?.Enter();
+    }
+}
