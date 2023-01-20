34a49e2eb2020dbfb25a9464ecd880dd091501d0
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2023-01-20 02:11:48 -0500

    Add logic for reading player input

--- Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace StateMachines.Player
 {
     public class PlayerStateMachine : StateMachine
     {
+        // Public variables
+        [field: SerializeField] public InputReader InputReader { get; private set; }
+
         // Unity built-in methods
         private void Start()
         {
             SwitchState(new PlayerTestState(this));
         }
+        
+        // Private methods
+        
     }
 }
-
--- Assets/Scripts/StateMachines/Player/PlayerTestState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerTestState.cs
@@ -5,30 +5,32 @@ namespace StateMachines.Player
 {
     public class PlayerTestState : PlayerBaseState
     {
-        private float _timer = 5;
-        
+        private float _timer = 0;
+
         public PlayerTestState(PlayerStateMachine stateMachine) : base(stateMachine)
         {
         }
 
         public override void Enter()
         {
-            Debug.Log("Enter");
+            stateMachine.InputReader.JumpEvent += OnJump;
         }
 
         public override void Tick(float deltaTime)
         {
-            _timer -= deltaTime;
+            _timer += deltaTime;
+
             Debug.Log(_timer);
-            if (_timer <= 0)
-            {
-                stateMachine.SwitchState(new PlayerTestState(stateMachine));
-            }
         }
 
         public override void Exit()
         {
-            Debug.Log("Exit");
+            stateMachine.InputReader.JumpEvent -= OnJump;
+        }
+
+        private void OnJump()
+        {
+            stateMachine.SwitchState(new PlayerTestState(stateMachine));
         }
     }
 }
