db08985207c4c4dedbbe891a428088212317005d
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2023-01-23 02:23:58 -0500

    Add health and damage scripts / components

---
+++ Assets/Scripts/Combat/WeaponDamage.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Combat
+{
+    public class WeaponDamage : MonoBehaviour
+    {
+        [SerializeField] private Collider myCollider;
+
+        private List<Collider> _alreadyCollidedWith = new();
+
+        private void OnEnable()
+        {
+            _alreadyCollidedWith.Clear();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other == myCollider) return;
+
+            if (_alreadyCollidedWith.Contains(other)) return;
+
+            _alreadyCollidedWith.Add(other);
+
+            if (other.TryGetComponent<Health>(out var health))
+            {
+                health.DealDamage(10);
+            }
+        }
+    }
+}
