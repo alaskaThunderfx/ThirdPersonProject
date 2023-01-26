26be42d8954f30d4311e69f60e31e2609c0b118c
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2023-01-26 00:00:37 -0500

    Add chasing range to enemy, and gizmo to show range

--- Assets/Scripts/StateMachines/Enemy/EnemyBaseState.cs
+++ Assets/Scripts/StateMachines/Enemy/EnemyBaseState.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace StateMachines.Enemy
 {
     public abstract class EnemyBaseState : State
@@ -7,6 +9,24 @@ namespace StateMachines.Enemy
         public EnemyBaseState(EnemyStateMachine stateMachine)
         {
             this.stateMachine = stateMachine;
+        }
+        
+        protected void Move(float deltaTime)
+        {
+            Move(Vector3.zero, deltaTime);
+        }
+
+        protected void Move(Vector3 motion, float deltaTime)
+        {
+            stateMachine.CharacterController.Move(
+                (motion + stateMachine.ForceReceiver.Movement) * deltaTime);
+        }
+
+        protected bool IsInChaseRange()
+        {
+            var playerDistanceSquare = (stateMachine.Player.transform.position - stateMachine.transform.position).sqrMagnitude;
+
+            return playerDistanceSquare <= stateMachine.PlayerChasingRange * stateMachine.PlayerChasingRange;
         }
     }
 }
