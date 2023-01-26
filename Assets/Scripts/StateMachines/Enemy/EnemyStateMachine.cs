aaaa875f4fa0c1da4eeb33d42c3eaf7e46718c70
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2023-01-25 23:46:47 -0500

    Set up basic animation control for enemy

--- Assets/Scripts/StateMachines/Enemy/EnemyStateMachine.cs
+++ Assets/Scripts/StateMachines/Enemy/EnemyStateMachine.cs
@@ -1,7 +1,16 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
 namespace StateMachines.Enemy
 {
     public class EnemyStateMachine : StateMachine
     {
-    
+        [field: SerializeField] public Animator Animator { get; private set; }
+
+        private void Start()
+        {
+            SwitchState(new EnemyIdleState(this));
+        }
     }
 }
